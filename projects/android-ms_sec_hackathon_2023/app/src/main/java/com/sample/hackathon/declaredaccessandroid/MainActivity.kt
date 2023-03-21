2d2e6e9649bb24500e3c59c972576dfbea891e5c
Sammy <sammy.odenhoven@rubicoin.com>
2023-03-21 21:46:10 +0000

    Merge of separate Compose & interceptor projects (and updated Gradle version)

--- projects/android-ms_sec_hackathon_2023/app/src/main/java/com/sample/hackathon/declaredaccessandroid/MainActivity.kt
+++ projects/android-ms_sec_hackathon_2023/app/src/main/java/com/sample/hackathon/declaredaccessandroid/MainActivity.kt
@@ -1,15 +1,62 @@
 package com.sample.hackathon.declaredaccessandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.sample.hackathon.declaredaccessandroid.navigation.*
+import com.sample.hackathon.declaredaccessandroid.ui.SplashScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // See MsalPublicClientFactory for a convenience function to init MSAL
-        // See GraphServiceFactory for convenience functions to call Graph
-
+        setContent {
+            Surface {
+                val navController = rememberNavController()
+                Scaffold(
+                    topBar = { AppBar() }
+                ) { innerPadding ->
+                    AuthNavHost(
+                        navController,
+                        modifier = Modifier.padding(innerPadding),
+                        splashScreenId = RootNav.SPLASH_SCREEN,
+                        splashScreen = {
+                            composable(RootNav.SPLASH_SCREEN) {
+                                SplashScreen()
+                            }
+                        },
+                        protectedRoutesRootId = ProtectedRoutesNav.ROUTE,
+                        protectedRoutes = { protectedRoutesGraph(navController) },
+                        unprotectedRoutesRootId = UnprotectedRoutesNav.ROUTE,
+                        unprotectedRoutes = { unprotectedRoutesGraph(navController) }
+                    )
+                }
+            }
+        }
     }
+}
+
+@Composable
+private fun AppBar() {
+    TopAppBar(
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Visibility,
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 12.dp)
+            )
+        },
+        title = {
+            Text(text = "Keep an eye on it")
+        },
+        backgroundColor = MaterialTheme.colors.primarySurface
+    )
 }
---
+++ projects/android-ms_sec_hackathon_2023/app/src/main/java/com/sample/hackathon/declaredaccessandroid/navigation/IntroNav.kt
@@ -0,0 +1,15 @@
+package com.sample.hackathon.declaredaccessandroid.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import com.sample.hackathon.declaredaccessandroid.ui.WelcomeScreen
+
+fun NavGraphBuilder.unprotectedRoutesGraph(navController: NavController) {
+    navigation(startDestination = UnprotectedRoutesNav.WELCOME_SCREEN, route = UnprotectedRoutesNav.ROUTE) {
+        composable(UnprotectedRoutesNav.WELCOME_SCREEN) {
+            WelcomeScreen(navController)
+        }
+    }
+}

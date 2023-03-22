74244c8cdc9d2bf79f0f0ee7656e9f4285dab9ff
Brian Melton-Grace <brianmel@microsoft.com>
2023-03-22 16:55:10 -0700

    Remove unused method

--- projects/android-ms_sec_hackathon_2023/app/src/main/java/com/sample/hackathon/declaredaccessandroid/graph/GraphService.java
+++ projects/android-ms_sec_hackathon_2023/app/src/main/java/com/sample/hackathon/declaredaccessandroid/graph/GraphService.java
@@ -4,24 +4,9 @@ import com.sample.hackathon.declaredaccessandroid.graph.dto.User;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Headers;
 
 public interface GraphService {
-
-    /**
-     * Calls the /me endpoint.
-     *
-     * @param bearerToken The bearer access token for this request.
-     * @return The serialized User object.
-     */
-    @Headers({
-            "Accept: application/json"
-    })
-    @GET("me")
-    Call<User> getMe(
-            @Header("Authorization") String bearerToken
-    );
 
     /**
      * Calls the /me endpoint.
